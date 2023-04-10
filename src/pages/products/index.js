c8a3fc493e72da76f6e031d636ba112cb48d0c86
aishat-liasu <aishatliasu17965@gmail.com>
2023-04-10 23:47:27 +0100

    refactor: remove redundant import

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Head from 'next/head';
 import styles from '@/styles/products.module.css';
-import Layout from '@/components/layout';
 
 export default function ProductPage() {
   const iconStyle = {
